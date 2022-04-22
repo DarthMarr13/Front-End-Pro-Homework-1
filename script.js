769745add2a77ae61ee0b89f36f380b392c3959b
Sergii Zlishchev <47575515+DarthMarr13@users.noreply.github.com>
2022-04-22 08:14:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Hello")
+let name = prompt('What is your name?');
+alert(`Nice to meet you, ${name}!`);
+let age = prompt('How old are you?');
+let year = new Date().getFullYear();
+alert(`You was born in year ${year - age} or ${year - age -1}, depends on mounth you were born`);
